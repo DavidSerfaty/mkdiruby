211fd22f621484763fbf40bce1c058cd7d495599
DavidSerfaty <david@owanted.io>
2020-10-18 13:37:54 +0200

    final commit

--- mkdiruby.rb
+++ mkdiruby.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 def check_if_user_gave_input
-  abort("mkdir: missing input") if ARGV.empty?
+  abort('mkdir: missing input') if ARGV.empty?
 end
 
 def create_folder(name)
@@ -11,7 +13,7 @@ def create_lib_folder(folder_name)
 end
 
 def create_gem_file(folder_name)
-  file = File.open("#{folder_name}/Gemfile", "a")
+  file = File.open("#{folder_name}/Gemfile", 'a')
   file.puts("source 'https://rubygems.org'")
   file.puts("
 ruby '2.7.1'
@@ -23,40 +25,40 @@ gem 'dotenv'")
 end
 
 def create_gitignore_and_env_file(folder_name)
-  file = File.open("#{folder_name}/.env", "a")
+  file = File.open("#{folder_name}/.env", 'a')
   file.close
-  file = File.open("#{folder_name}/.gitignore", "a")
-  file.puts(".env")
+  file = File.open("#{folder_name}/.gitignore", 'a')
+  file.puts('.env')
   file.close
 end
 
 def command_init(folder_name)
-  Dir.chdir("#{folder_name}") {
-    system("git init")
-    system("bundle install")
-    system("rspec --init")
-   }
+  Dir.chdir(folder_name.to_s) do
+    system('git init')
+    system('bundle install')
+    system('rspec --init')
+  end
 end
 
 def create_read_me_file(folder_name)
-  file = File.open("#{folder_name}/Readme.md", "a")
-  file.puts("Ceci est un programme Ruby")
+  file = File.open("#{folder_name}/Readme.md", 'a')
+  file.puts('Ceci est un programme Ruby')
   file.close
 end
 
 def command_git(folder_name)
-  Dir.chdir("#{folder_name}") {
-    system("git add .")
+  Dir.chdir(folder_name.to_s) do
+    system('git add .')
     system('git commit -m "first commit"')
-   }
+  end
 end
 
 def create_app_rb_file(folder_name)
-  file = File.open("#{folder_name}/lib/app.rb", "a")
-  file.puts("Hello mon boug !")
+  file = File.open("#{folder_name}/lib/app.rb", 'a')
+  file.puts('Hello mon boug !')
   file.close
 
-  file = File.open("#{folder_name}/spec/app_spec.rb", "a")
+  file = File.open("#{folder_name}/spec/app_spec.rb", 'a')
   file.puts("Hey mon boug, n'oublies pas tes specs !")
   file.close
 end
@@ -76,4 +78,4 @@ end
 
 get_folder_name
 
-#alias mkdiruby="ruby /Users/david_sfy/Desktop/THP/mkdiruby.rb"
+# alias mkdiruby="ruby /Users/david_sfy/Desktop/THP/mkdiruby.rb"
